509f5608c982e7977dfe132b31a22d5aad93b497
Victor Hugo Tello <vctrtello@icloud.com>
2026-03-05 10:54:05 -0600

    Single number

--- SingleNumber/Main.java
+++ SingleNumber/Main.java
@@ -8,6 +8,7 @@ public class Main {
         int result = main.singleNumber(nums);
         System.out.println(result);
     }
+    // XOR operation has a property that a ^ a = 0 and a ^ 0 = a. Therefore, if we XOR all the numbers in the array, the pairs will cancel each other out, leaving us with the single number that appears only once.
 
     // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
     public int singleNumber(int[] nums) {
