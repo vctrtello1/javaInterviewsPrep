d2c530b55418a3b55c84cdf75f0a6e3aa08ba91c
Victor Hugo Tello <vctrtello@icloud.com>
2026-03-05 15:50:31 -0600

    Roman to Integer

--- RomanToInteger/Main.java
+++ RomanToInteger/Main.java
@@ -1,19 +1,29 @@
 package RomanToInteger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-    // Symbol       Value
-    // I             1
-    // V             5
-    // X             10
-    // L             50
-    // C             100
-    // D             500
-    // M             1000
-    // For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-    // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-    // I can be placed before V (5) and X (10) to make 4 and 9. 
-    // X can be placed before   L (50) and C (100) to make 40 and 90.   
+    // Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+    // and M.
+    // Symbol Value
+    // I 1
+    // V 5
+    // X 10
+    // L 50
+    // C 100
+    // D 500
+    // M 1000
+    // For example, 2 is written as II in Roman numeral, just two one's added
+    // together. 12 is written as XII, which is simply X + II. The number 27 is
+    // written as XXVII, which is XX + V + II.
+    // Roman numerals are usually written largest to smallest from left to right.
+    // However, the numeral for four is not IIII. Instead, the number four is
+    // written as IV. Because the one is before the five we subtract it making four.
+    // The same principle applies to the number nine, which is written as IX. There
+    // are six instances where subtraction is used:
+    // I can be placed before V (5) and X (10) to make 4 and 9.
+    // X can be placed before L (50) and C (100) to make 40 and 90.
 
     // C can be placed before D (500) and M (1000) to make 400 and 900.
     public static void main(String[] args) {
@@ -25,35 +35,25 @@ public class Main {
     }
 
     public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
         int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            int currentValue = getValue(current);
-            if (i + 1 < s.length()) {
-                char next = s.charAt(i + 1);
-                int nextValue = getValue(next);
-                if (currentValue < nextValue) {
-                    result -= currentValue; // Subtract if the current value is less than the next value
-                } else {
-                    result += currentValue; // Otherwise, add the current value
-                }
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
             } else {
-                result += currentValue; // Add the last character's value
+                result += map.get(s.charAt(i));
             }
         }
         return result;
     }
 
-    private int getValue(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
-    }
 }
